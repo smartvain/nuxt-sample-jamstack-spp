b8c72f82527456cc6546df8a5afb82dfb6cfb8eb
smartvain <zlc.blue15210@gmail.com>
2022-12-03 15:26:00 +0900

    refactor: config property order

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,21 @@
 const { API_KEY, SERVICE_DOMAIN } = process.env
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/nuxt-sample-jamstack-spp/',
+    cdnURL: 'https://smartvain.github.io/nuxt-sample-jamstack-spp/',
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
   runtimeConfig: {
     apiKey: API_KEY,
     serviceDomain: SERVICE_DOMAIN,
     public: {
       apiKey: process.env.NODE_ENV !== 'production' ? API_KEY : undefined,
       serviceDomain: process.env.NODE_ENV !== 'production' ? SERVICE_DOMAIN : undefined,
-    },
-  },
-  app: {
-    cdnURL: '/nuxt-sample-jamstack-spp/',
-    head: {
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ]
     }
   }
 })
